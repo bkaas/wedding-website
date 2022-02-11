54057da27d4adfff8213e03623f36a43ab872109
Brendan Kaas <bren.kaas@gmail.com>
2022-02-10 21:45:49 -0500

    working fold, cleanup required

---
+++ src/pages/savethedate.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Envelope from "../components/Envelope.js"
+
+const SaveTheDate = () => {
+
+  return (
+    <main>
+      <title>Save The Date Page</title>
+      <h1>Save The Date!</h1>
+      <Envelope />
+    </main>
+  );
+
+}
+
+export default SaveTheDate
