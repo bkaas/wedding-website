48e6528e9956a4fe6054bdd0ec5e203f54fd1fe0
Brendan Kaas <bren.kaas@gmail.com>
2022-11-05 20:08:35 -0400

    text formatting components

---
+++ src/components/Layout/textformat.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const LineBreak = styled.span`
+  display: block;
+  margin-bottom: 1em;
+`
+export const IndentedText = styled.span`
+  display: block;
+  margin-left: 3%;
+`
